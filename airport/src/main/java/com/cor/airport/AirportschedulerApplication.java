644963e2dff86d16f94304417a5f81767efaa509
CoeWorl <cole.a.wissink@gmail.com>
2025-04-01 14:35:39 -0400

    Creating springboot project using Spring Initializr

---
+++ airport/src/main/java/com/cor/airport/AirportschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.cor.airport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportschedulerApplication.class, args);
+	}
+
+}
---
+++ airport/src/test/java/com/cor/airport/AirportschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cor.airport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
